76a1c98425abe1a6b98ca5121ba7949037dadb21
ReverieTides <reverietides@gmail.com>
2018-11-10 16:56:49 +0000

    Completed Task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This file contains two functions, one for storing data inside a function/cache
+#whilst the other is for recalling that information
 
-## Write a short comment describing this function
+#makeCacheMatrix will create list of several functions which will allow the
+#functions to 'carry around' data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invmtrx <- NULL
+        set <- function(y) {
+            x <<- y
+            invmtrx <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve)invmtrx <<- solve
+        getinv <- function() invmtrx
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will check to see if the calculation has been performed before
+#and if so, it will recall the data from the cache as opposed to a new calculation
+#if not, it will perform solve on x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        invmtrx <- x$getinv()
+        if(!is.null(invmtrx)) {
+            return(invmtrx)
+        }
+        result <- x$get()
+        invmtrx<-solve(result)
+        x$setinv(invmtrx)
+        invmtrx
+    }
+    
